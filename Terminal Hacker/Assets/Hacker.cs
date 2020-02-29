fd93436c01cf7da8944e3d6c342ad4d483d46227
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-02-28 17:51:24 -0700

    -Backup Commit.

--- Terminal Hacker/Assets/Hacker.cs
+++ Terminal Hacker/Assets/Hacker.cs
@@ -1,25 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    //Game State
+    int level;
+
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen = Screen.MainMenu;
+
     // Start is called before the first frame update
     void Start()
     {
         ShowMainMenu();
-    }
-
-    private static void ShowMainMenu()
-    {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Welcome to Terminal hacker!");
-        Terminal.WriteLine("What would you like to do?");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("1. Hack Home PCs");
-        Terminal.WriteLine("2. Hack Remote Servers");
-        Terminal.WriteLine("3. Hack Government Systems");
-        Terminal.WriteLine("Enter your selection:");
     }
 
     // Update is called once per frame
@@ -34,25 +29,58 @@ public class Hacker : MonoBehaviour
         {
             ShowMainMenu(); 
         }
-        else if(input == "1")
+        else if(currentScreen == Screen.MainMenu)
         {
-            print("You chose level 1");
+            RunMainMenu(input);
+        }
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Welcome to Terminal hacker!");
+        Terminal.WriteLine("What would you like to do?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("1. Hack Home PCs");
+        Terminal.WriteLine("2. Hack Remote Servers");
+        Terminal.WriteLine("3. Hack Government Systems");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            currentScreen = Screen.Password;
+            StartLevel();
         }
         else if (input == "2")
         {
-            print("You chose level 2");
+            level = 2;
+            currentScreen = Screen.Password;
+            StartLevel();
         }
         else if (input == "3")
         {
-            print("You chose level 3");
+            level = 3;
+            currentScreen = Screen.Password;
+            StartLevel();
         }
-        else if(input == "secretsecret")
+        else if (input == "supersecret")
         {
-            print("Your secret is safe with me.");
+            Terminal.WriteLine("Your secret is safe with me");
         }
         else
         {
-            print("Please select a valid level");
+            Terminal.WriteLine("Please select a valid level.");
         }
+    }
+
+    void StartLevel()
+    {
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
     }
 }
